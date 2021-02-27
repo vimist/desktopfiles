d331d8a3cd5506c9b553901260cb1487e1dbff06
vimist <vimist@users.noreply.github.com>
2021-02-27 23:21:14 +0000

    Updated install packages script

--- install_packages.sh
+++ install_packages.sh
@@ -15,19 +15,32 @@ inst_pkg xdo
 inst_pkg alacritty
 inst_pkg ncdu      # Disk usage analyser
 inst_pkg scrot
+inst_pkg playerctl
 
 
-inst_pip git+https://github.com/vimist/watch-do
+inst_pip watch-do
+inst_pip lemonbar-manager
 
 
-# Instsall xtmon
 pushd /tmp
-	git clone https://github.com/vimist/xtmon
+    # Instsall xtmon
+    git clone https://github.com/vimist/xtmon
 
-	pushd xtmon
-		make
-		sudo make install
-	popd
+    pushd xtmon
+        make
+        sudo make install
+    popd
 
-	rm -Rf xtmon
+    rm -Rf xtmon
+
+
+    # Install colorpicker
+    git clone https://github.com/Jack12816/colorpicker
+
+    pushd colorpicker
+        make
+        sudo install colorpicker /usr/local/bin
+    popd
+
+    rm -Rf xtmon
 popd
