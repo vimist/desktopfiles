a146418fe06f8688157a19477d17da83e2f1c0ee
vimist <vimist@users.noreply.github.com>
2021-02-27 23:12:15 +0000

    Moved all bspwm window rules to external script

--- files/bspwm/external_rules_command.sh
+++ files/bspwm/external_rules_command.sh
@@ -8,7 +8,13 @@ TITLE="$(xtitle "$WID")"
 
 if [[ "$CLASS" = "Blender" && "$TITLE" = "Blender Preferences" ]]; then
 	echo 'state=floating center'
+elif [[ "$CLASS" = "Steam" && "$TITLE" != "Steam" ]]; then
+	echo 'state=floating center'
+elif [[ "$CLASS" = "feh" ]]; then
+	echo 'state=pseudo_tiled center'
+elif [[ "$CLASS" = "mpv" ]]; then
+	echo 'state=pseudo_tiled center'
 fi
 
 # Debug - `tail -f /tmp/bspwm_rules_script`
-#echo "$WID, $CLASS, $INSTANCE, $TITLE" >> /tmp/bspwm_rules_script
+# echo "$WID, $CLASS, $INSTANCE, $TITLE" >> /tmp/bspwm_rules_script
