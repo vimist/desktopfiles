0b46661c250dee2cf0a804df4b1761326b00dc2b
vimist <vimist@users.noreply.github.com>
2021-02-27 23:38:53 +0000

    Refactor of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,51 +1,61 @@
-#! /usr/bin/env bash
+#! /bin/sh
 
 # Ensure we're working from this directory
 cd "$(dirname $0)"
 
-# The link function will append this to any files that already
-# exist in order to preserve them
-backup_suffix='.orig'
+# Output colours
+error() { tput setaf 1; echo "$@"; tput sgr0; }
+warn() { tput setaf 3; echo "$@"; tput sgr0; }
+success() { tput setaf 2; echo "$@"; tput sgr0; }
 
-# Link a file from this repository into the users home directory
-# backups of existing files will be made with a suffix of $backup_suffix
+# Link a file from $1 to $2 with some nice checks
 # $1 The source file, relative to the current directory
-# $2 The destination file, relative to $HOME/.config
-#    $1 is used if $2 is not specified, this allows us to pass in a single
-#    argument if $1 should be recreated under $HOME
-function link() {
-    default_dst="$(basename "$PWD")/$1"
+# $2 The destination file
+#    If unspecified, the basename of the current directory will be appended to
+#    "$HOME/.config" and the file will be linked there.
+#    If the destination is specified then it will be used.
+link() {
+    default_dst="$HOME/.config/$(basename "$PWD")/$1"
 
     src="$PWD/$1"
-    dst="$HOME/.config/${2:-$default_dst}"
+    dst="${2:-$default_dst}"
+
+    # Check source exists
+    if [[ ! -e "$src" ]]; then
+        error "Source file '$src' does not exist"
+        return
+    fi
 
     mkdir -p "$(dirname "$dst")"
 
-    # If the destination is a symlink
-    if [[ -L "$dst" ]]; then
-        rm "$dst"
-        echo "Removed existing symlink '$dst'"
+    # If the destination exists
+    if [[ -e "$dst" || -L "$dst" ]]; then
+        warn -n "Removing existing file '$dst'... "
+        if rm "$dst"; then
+            success Done
+        else
+            error Failed
+            return
+        fi
     fi
 
-    ln --symbolic --backup --suffix "$backup_suffix" "$src" "$dst"
-    echo "Installed '$dst'"
+    if ln --symbolic "$src" "$dst"; then
+        success "Installed '$src'"
+    else
+        error "Could not symlink '$src'"
+    fi
 }
 
 # Redirect the output so we don't clutter the screen
-function pushd() {
-    command pushd "$1" > /dev/null
-}
+pushd() { command pushd "$1" > /dev/null; }
+popd() { command popd > /dev/null; }
 
-# Redirect the output so we don't clutter the screen
-function popd() {
-    command popd > /dev/null
-}
 
 # Start linking files
 
 # General files
 pushd files
-link bg.jpg bg.jpg
+link bg.jpg "$HOME/.config/bg.jpg"
 popd
 
 # BSPWM
@@ -68,9 +78,10 @@ link clock_modules.py
 link disk_modules.py
 link sound_modules.py
 link task_modules.py
+link media_modules.py
 popd
 
-# dmenu
+# Dmenu
 pushd files/dmenu
 link base.sh
 link launcher.sh
@@ -83,17 +94,16 @@ pushd files/dunst
 link dunstrc
 popd
 
-# feh
+# Feh
 pushd files/feh
 link themes
 popd
 
 # Scripts
 pushd files/scripts
-link window_autotype.sh
-link system_update.sh
+link window_autotype.sh "$HOME/bin/window_autotype"
+link system_update.sh "$HOME/bin/system_update"
 popd
 
 echo
 echo "Done!"
-echo "Any existing files have been renamed with a suffix of '$backup_suffix'"
