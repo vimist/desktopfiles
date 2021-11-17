3bec743d751d3884c07f3144eae064e9674991ed
vimist <vimist@users.noreply.github.com>
2021-11-17 12:12:38 +0000

    Changed scrot parameters - no functionality difference
    Maybe an update or something changed the way scrot handles args

--- files/scripts/screenshot.sh
+++ files/scripts/screenshot.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-scrot -so /tmp/screenshot.png
+scrot -s -o /tmp/screenshot.png
 xclip -selection clipboard -target image/png /tmp/screenshot.png
 dunstify 'Screenshot' "Saved in '/tmp/screenshot.png' and copied to the clipboard"
