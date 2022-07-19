15783179608b0531fe74600d09a8c3d4f12624a2
vimist <vimist@users.noreply.github.com>
2022-07-19 21:40:43 +0100

    Omit border when taking screenshot

--- files/scripts/screenshot.sh
+++ files/scripts/screenshot.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-scrot -s -o /tmp/screenshot.png
+scrot -s -l mode=edge -o /tmp/screenshot.png
 xclip -selection clipboard -target image/png /tmp/screenshot.png
 dunstify 'Screenshot' "Saved in '/tmp/screenshot.png' and copied to the clipboard"
